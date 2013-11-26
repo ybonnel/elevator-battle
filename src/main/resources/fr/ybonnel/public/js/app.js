7e24c803ec4b24541da27be990b3e01dd7afb77e
ybonnel <ybonnel@gmail.com>
2013-11-26 14:59:43 +0100

    Fuck browser cache

--- src/main/resources/fr/ybonnel/public/js/app.js
+++ src/main/resources/fr/ybonnel/public/js/app.js
@@ -1,10 +1,10 @@
 angular.module('BattleApp', ['ui.bootstrap'], function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'partials/main.html',
+            templateUrl: 'partials/main.html?version=1.0.0',
             controller: MainCtrl})
         .when('/battle/:pseudo1/:pseudo2', {
-            templateUrl: 'partials/battle.html',
+            templateUrl: 'partials/battle.html?version=1.0.0',
             controller: BattleCtrl})
         .otherwise({redirectTo: '/'});
 });
